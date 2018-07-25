d7205cb49a39b93ede2418916159507b0c45a6e1
zhongzhi107 <zhong.zhi@163.com>
2018-07-25 19:39:17 +0800

    release: v1.0.1

--- index.js
+++ index.js
@@ -1,26 +1,15 @@
 var preset = {
   presets: [
     [require.resolve('babel-preset-env'), { modules: false }],
-    require.resolve('babel-preset-react'),
+    require.resolve('babel-preset-react')
   ],
   plugins: [
     require.resolve('babel-plugin-add-module-exports'),
     require.resolve('babel-plugin-transform-class-properties'),
-
-    // The following two plugins use Object.assign directly, instead of Babel's
-    // extends helper. Note that this assumes `Object.assign` is available.
-    // { ...todo, completed: true }
-    [
-      require.resolve('babel-plugin-transform-object-rest-spread'),
-      {
-        useBuiltIns: true,
-      },
-    ],
-    // Adds syntax support for import()
-    // require.resolve('babel-plugin-syntax-dynamic-import'),
-    // Add support for async/await
+    [require.resolve('babel-plugin-transform-object-rest-spread'), { useBuiltIns: true }],
     require.resolve('babel-plugin-transform-runtime'),
-  ],
+    process.env.NODE_ENV === 'production' && require('babel-plugin-transform-react-remove-prop-types')
+  ].filter(Boolean)
 };
 
 module.exports = preset;
