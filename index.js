d2c2e73d1eb14ed0b02ae8d4c5320a9b501a23a4
zhongzhi107 <zhong.zhi@163.com>
2018-07-26 15:10:14 +0800

    fix

--- index.js
+++ index.js
@@ -7,8 +7,11 @@ var preset = {
     require.resolve('babel-plugin-add-module-exports'),
     require.resolve('babel-plugin-transform-class-properties'),
     [require.resolve('babel-plugin-transform-object-rest-spread'), { useBuiltIns: true }],
+
+    // 支持 import() 语法
+    require.resolve('babel-plugin-syntax-dynamic-import'),
     require.resolve('babel-plugin-transform-runtime'),
-    process.env.NODE_ENV === 'production' && require('babel-plugin-transform-react-remove-prop-types').default
+    process.env.NODE_ENV === 'production' && require.resolve('babel-plugin-transform-react-remove-prop-types')
   ].filter(Boolean)
 };
 
